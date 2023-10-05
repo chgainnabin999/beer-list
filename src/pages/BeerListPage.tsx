fb3e59b66e2e4ce56c2e76d928ead23a96c197bb
nabinchaulagain <nabinchaulagain@lftechnology.com>
2023-10-05 08:03:14 +0545

    fix rendering of load more beers icon.

--- src/pages/BeerListPage.tsx
+++ src/pages/BeerListPage.tsx
@@ -10,54 +10,52 @@ import { BeerContext } from 'src/context/Beer';
 import { BeerListTabChoice } from 'src/types/Beer';
 
 const BeerListPage: React.FC = () => {
-    const [isAddBeerModalOpen, setIsAddBeerModalOpen] = useState(false);
-    const beerContext = useContext(BeerContext);
+  const [isAddBeerModalOpen, setIsAddBeerModalOpen] = useState(false);
+  const beerContext = useContext(BeerContext);
 
-    const loadMoreBeers = beerContext?.hasMoreBeers ? (
-        <button
-            className="text-blue-700 font-bold align-middle flex hover:text-blue-500"
-            onClick={() => {
-                beerContext?.loadMoreBeers();
-            }}
-        >
-            Load more
-            <div className="text-2xl ml-2 -translate-y-1">
-                {LOAD_MORE_BEERS_ICON_UNICODE}
+  const loadMoreBeers = beerContext?.hasMoreBeers ? (
+    <button
+      className="text-blue-700 font-bold align-middle flex hover:text-blue-500"
+      onClick={() => {
+        beerContext?.loadMoreBeers();
+      }}
+    >
+      Load more
+      <div className="text-2xl ml-2 -translate-y-1" dangerouslySetInnerHTML={{ __html: LOAD_MORE_BEERS_ICON_UNICODE }}></div>
+    </button>
+  ) : null;
+
+  return (
+    <>
+      <div>
+        <div className="flex pt-3 pl-5">
+          <BeerListTabs />
+          {beerContext?.selectedTab === BeerListTabChoice.MY_BEERS ? (
+            <div className="ml-auto mr-4">
+              <Button onClick={() => setIsAddBeerModalOpen(true)}>
+                Add a new beer
+              </Button>
             </div>
-        </button>
-    ) : null;
+          ) : null}
+        </div>
 
-    return (
-        <>
-            <div>
-                <div className="flex pt-3 pl-5">
-                    <BeerListTabs />
-                    {beerContext?.selectedTab === BeerListTabChoice.MY_BEERS ? (
-                        <div className="ml-auto mr-4">
-                            <Button onClick={() => setIsAddBeerModalOpen(true)}>
-                                Add a new beer
-                            </Button>
-                        </div>
-                    ) : null}
-                </div>
-
-                {!beerContext?.beers?.length &&
-                beerContext?.isLoading ? null : (
-                    <BeerList beers={beerContext?.beers || []} />
-                )}
-                <div className="flex justify-center pb-2">
-                    {beerContext?.isLoading ? <Loader /> : loadMoreBeers}
-                </div>
-            </div>
-            <Modal
-                isOpen={isAddBeerModalOpen}
-                title="Add a New Beer"
-                setIsOpen={setIsAddBeerModalOpen}
-            >
-                <AddBeerForm closeModal={() => setIsAddBeerModalOpen(false)} />
-            </Modal>
-        </>
-    );
+        {!beerContext?.beers?.length &&
+          beerContext?.isLoading ? null : (
+          <BeerList beers={beerContext?.beers || []} />
+        )}
+        <div className="flex justify-center pb-2">
+          {beerContext?.isLoading ? <Loader /> : loadMoreBeers}
+        </div>
+      </div>
+      <Modal
+        isOpen={isAddBeerModalOpen}
+        title="Add a New Beer"
+        setIsOpen={setIsAddBeerModalOpen}
+      >
+        <AddBeerForm closeModal={() => setIsAddBeerModalOpen(false)} />
+      </Modal>
+    </>
+  );
 };
 
 export default BeerListPage;
